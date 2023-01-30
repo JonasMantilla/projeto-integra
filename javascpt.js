7eeea7b8b8809a2f28be03bd36ff9f5e81b8560e
Jonas Mantilla <darknumbervr@gmail.com>
2023-01-30 11:37:31 -0300

    botao  vermais

--- javascpt.js
+++ javascpt.js
@@ -21,4 +21,19 @@ const relogio = setInterval(function time() {
 })
 
 
+function vermais(n){
+  let pontos=document.getElementById("pontos"+n);
+  let maisTexto=document.getElementById("mais"+n);
+  let btnVermais=document.getElementById("btnVerMais"+n);
+
+  if(pontos.style.display === "none"){
+    pontos.style.display="inline";
+    maisTexto.style.display="none";
+    btnVermais.innerHTML="Ver mais";
+  }else{
+    pontos.style.display="none";
+    maisTexto.style.display="inline";
+    btnVermais.innerHTML="Ver menos";
+  }
+}
 
